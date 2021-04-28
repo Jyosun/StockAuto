94ce3583d5766c3c4b3b34aaba5189530a581346
jyosun <jys000305@naver.com>
2021-04-29 08:56:32 +0900

    upload files

---
+++ AutoConnect.py
@@ -0,0 +1,13 @@
+from pywinauto import application
+import time
+import os
+
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+time.sleep(5)        
+
+app = application.Application()
+app.start('D:\STARTER\coStarter.exe /prj:cp /id:yourid /pwd:yourpwd /pwdcert:yourpwdcert /autostart')
+time.sleep(60)
